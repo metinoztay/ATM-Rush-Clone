0acb488aeebbaac767610ddf5e4f881ea39a106a
Metin ÖZTAY <metinoztay@gmail.com>
2022-12-09 11:16:04 +0300

    ATM Counter Script

--- ATM Rush Clone/Assets/Scripts/ATMController.cs
+++ ATM Rush Clone/Assets/Scripts/ATMController.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ATMController : MonoBehaviour
 {
+    [SerializeField] TextMeshPro counter;
+    private int count;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Collected"))
         {
             Destroy(other.gameObject);
-            MoneyCounter.count += 1;
+            count++;
+            
             
         }
         else if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        counter.text = count.ToString();
     }
 }
